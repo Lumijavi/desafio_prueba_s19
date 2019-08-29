2c9c36dc04c2d276a20c6eea9697f4508fb7432d
lumijavi <basturn@gmail.com>
2019-08-28 21:47:42 -0400

    created tasks controller and rest routes, added root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+  resources :tasks, only: :index
+
   devise_for :users
 
   # devise_for :users, controllers: {
   # sessions: 'users/sessions'
   # }
 
-  # root to: "home#index"
+  root "tasks#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
