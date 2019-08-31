380486ca1d96039e748f781c95d123935bcd60de
lumijavi <basturn@gmail.com>
2019-08-31 03:32:25 -0400

    added name and photo attributes to user, updated taks show view users lists

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
     resources :completed_tasks, only: [:create, :destroy]
   end
 
-  devise_for :users
-
-  # devise_for :users, controllers: {
-  # sessions: 'users/sessions'
-  # }
+  devise_for :users, controllers: {
+  registrations: 'users/registrations'
+  }
 
   root "tasks#index"
 
