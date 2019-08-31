8cba65c0cc92ff0e395a895a6ca06034a95070b8
lumijavi <basturn@gmail.com>
2019-08-31 02:55:29 -0400

    added first 5 users and complete list of users that completed task to show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,8 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find(params[:id])
+    @top_five = CompletedTask.all.where(task_id: @task).order(:created_at).limit(5).pluck('user_id')
+    @completed_task_users_id = CompletedTask.all.where(task_id: @task).pluck('user_id')
   end
 
 
